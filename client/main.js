7e821ca71260d9e175d9da9be8aeb09f9e43985d
Justin Schneider <jmschneider2462@gmail.com>
2017-11-12 21:38:21 -0500

    Updated with react

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../imports/ui/App.jsx';
+
+Meteor.startup(() => {
+    render(<App />, document.getElementById('root'));
+});
