af60e89beaeede4441168adfc9f5616b136881bf
Justin Schneider <jmschneider2462@gmail.com>
2017-11-13 19:36:58 -0500

    Changed Login into one class instead of two, updated the Registration class

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -10,61 +10,60 @@ export default class Home extends React.Component {
     render() {
         return (
             <div>
-                <Header/>
                 <Login/>
             </div>
         )
     }
 }
 
-class Header extends React.Component {
-    render(){
-        return(
-            <div className = "mui--text-center">
-                <h1>Hydration Mangager</h1>
-                <br/>
-            </div>
-        )
-    }
-}
-
-{/* TODO: If only can figure out how to apply css to the Form and the Inputs so that it doesnt strech the entire screen */}
-{/* I put the entire thing in a container so it doesn't streach the entire screen*/}
-
 class Login extends React.Component {
     render() {
         return (
-            <div className="mui--text-center">
+            <div className = "mui--text-center">
+                <h1>Hydration Mangager</h1>
+                <br/>
                 <Container>
-                    <Form className="mui--text-left">
-                        <Input label="Email Address" type="email" floatingLabel={true} required={true}/>
-                        <Input label="Password" type="password" floatingLabel={true} required={true}/>
+                    <Form className = "mui--text-left" >
+                        <Input label = "Email Address" type = "email" floatingLabel = {true} required = {true} />
+                        <Input label = "Password" type = "password" floatingLabel = {true} required = {true} />
                     </Form>
                     <br/>
-                    <Button variant="raised">Login</Button>
-                    <Button variant="raised">Register</Button>
+                    <Button variant = "raised" >Login</Button>
+                    <Button variant = "raised">Register</Button>
                     <br/>
-                    <Button variant="raised">Forgot Password</Button>
+                    <Button variant = "raised">Forgot Password</Button>
                     <br/>
-                    <sub name="tagline">A University of South Carolina Capstone Project</sub>
+                    <sub name = "tagline">A University of South Carolina Capstone Project</sub>
                 </Container>
             </div>
         )
     }
 }
 
-/*
+
 class Registration extends React.Component {
     render() {
-        return (
-            <div className="mui--text-center">
-                <Container>
+        return(
+            <div>
+                <Container className = "mui--text-left">
+                    <h1>Registration</h1>
+                    <br/>
+                    <Form>
+                        <Input label = "Your Name" floatingLabel = {true} required = {true}/>
+                        <Input label = "Email Address" type = "email" floatingLabel = {true} required = {true} />
+                        <Input label = "Password" type = "password" floatingLabel = {true} required = {true} />
+                        <Input label = "Confirm Password" type = "password" floatingLabel = {true} required = {true} />
+                    </Form>
+                    <br/>
+                    <div className = "mui--text-right">
+                        <Button variant = "raised">Create Account</Button>
+                    </div>
                 </Container>
             </div>
         )
     }
 }
-
+/*
 class Forgotpassword extends React.Component {
     render() {
         return (
