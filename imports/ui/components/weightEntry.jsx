83b0b1dd171c468e467d45f3ed2b6cce076a0dc2
Tony Frazier <anthonyf@email.sc.edu>
2017-12-05 15:31:09 -0500

    Merged latest changes from online. Work done on weight entry page: working date selector and pre/post weight

--- imports/ui/components/weightEntry.jsx
+++ imports/ui/components/weightEntry.jsx
@@ -6,30 +6,30 @@ import { Modal } from 'react-bootstrap';
 import { ListGroup } from 'react-bootstrap';
 import { ListGroupItem } from 'react-bootstrap';
 
+
 export default class WeightEntry extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             selectedOption: 'PreWeight',
-            selectedDate: '12-08-2017'
-        }
+            selectedDate: '2017-12-08'
         };
+        console.log('The DEFAULT date is:', this.state.selectedDate);
+        console.log('You DEFAULT radio option is:', this.state.selectedOption);
 
-    handleOptionChange(changeEvent) {
-        this.state = ({
-            selectedOption: changeEvent.value
-        });
-        console.log('You have selected:', this.state.selectedOption);
+        this.handleOptionChange = this.handleOptionChange.bind(this);
+        this.handleDateChange =  this.handleDateChange.bind(this);
+    };
+
+    handleOptionChange = (e) => {
+        this.setState({selectedOption: e.target.value});
+        console.log('You have selected:', e.target.value);
+
     }
-    handleDateChange(changeEvent) {
-        this.state = ({
-            selectedDate: changeEvent.target.value
-        });
-        console.log('The date you selected is:', this.state.selectedDate);
-    }
-    handleFormSubmit(formSubmitEvent) {
-        console.log('The date you selected is:', this.state.selectedDate);
-        console.log('You have selected:', this.state.selectedOption);
+    handleDateChange = (e) => {
+        this.setState({selectedDate: e.target.value});
+        console.log('The date you selected is:', e.target.value);
+
     }
 
     render() {
@@ -40,22 +40,24 @@ export default class WeightEntry extends React.Component {
                 <form>
                     <div className="mui--text-center">
                         <br/><br/>
-                        <input type="date" value="selectedDate" onChange={this.handleDateChange}/>
+                        <input type="date" value={this.state.selectedDate} onChange={this.handleDateChange.bind(this)}/>
                         <br/><br/>
                         <div className="radio">
                             <label>
-                                <input type="radio" value="PreWeight" defaultChecked={this.state.selectedOption === "PreWeight"} onChange={this.handleOptionChange}/>
+                                <input type="radio" value="PreWeight" checked={this.state.selectedOption === 'PreWeight'} onChange={this.handleOptionChange.bind(this)}/>
                                 PreWeight
                             </label>
                             <label>
 
                             </label>
                             <label>
-                                <input type="radio" value="PostWeight" checked={this.state.selectedOption === "PostWeight"} onChange={this.handleOptionChange}/>
+                                <input type="radio" value="PostWeight" checked={this.state.selectedOption === 'PostWeight'} onChange={this.handleOptionChange.bind(this)}/>
                                 PostWeight
                             </label>
                         </div>
                         <br/><br/>
+
+
                             <Button variant="raised" onClick={this.handleFormSubmit}>Submit</Button>
                     </div>
                 </form>
