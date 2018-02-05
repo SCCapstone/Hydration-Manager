676c1f3ecd7c61242ea2093a88f1f1d1f74c5a16
Justin Schneider <jmschneider2462@gmail.com>
2018-02-04 21:16:34 -0500

    Attempt at unit testing

---
+++ imports/ui/components/unitTest.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default class unitTest extends Component {
+    sum(number1, number2){
+        return number1 + number2;
+    }
+    render() {
+        return (
+            <div>
+                {this.sum(this.props.num1, this.props.num2)}
+            </div>
+        );
+    }
+}
--- tests/athletes/add-athlete.tests.js
+++ tests/athletes/add-athlete.tests.js
@@ -1,3 +1,18 @@
+import React from 'react';
+import { chai } from 'meteor/practicalmeteor:chai';
+import { shallow } from 'enzyme';
+import unitTest from './unitTest';
+//import sum from './unitTest';
+
+describe("unitTest", function() {
+    it("sum", function() {
+        // chai.expect(2 + 2).to.equal(4);
+        // var result = sum(2+2);
+        // chai.assert.equal(result,'4','FeelsGoodMan');
+    });
+});
+
+//George's Attempt
 // describe( 'Add an athlete', function() {
 //     it( 'should create a new athlete @watch', function () {
 //         browser.url( 'http://localhost:3000/app/masterReport')
