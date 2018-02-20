b1bd26f374ce550299d6d1b325382b0d8a1be605
George Clayton <georgejc@email.sc.edu>
2018-02-20 13:09:48 -0500

    Added button to route back to master report from athlete report

--- imports/ui/components/athleteReport.jsx
+++ imports/ui/components/athleteReport.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Button } from 'react-bootstrap';
 import {Athletes} from '../../api/athletes.jsx';
+import {Teams} from '../../api/teams.jsx';
 import { Link } from 'react-router-dom';
 
 export default class AthleteReport extends Component {
@@ -24,14 +25,23 @@ export default class AthleteReport extends Component {
             return "";
         }
     }
+
+    team() {
+        playerTeamId = this.athlete().teamId;
+        team = Teams.findOne({"_id": playerTeamId});
+        return team;
+    }
+
     render() {
         athlete = this.athlete;
+        team = this.team;
         return (
             <div>
+                <Link to = {"/app/masterReport/" + this.team()._id}><Button bsStyle="primary">&lt; Back to {this.team().name} {this.team().season}</Button></Link>
                 <h3>Athlete Report</h3>
                 {/*TODO: Create component for the basic info*/}
                 <h4>{this.athlete().name}</h4>
-                <h4>{this.athlete().teamId}</h4>
+                <h4>{this.team().name} {this.team().season}</h4>
                 <h4>{this.athlete().height}</h4>
                 <h4>{this.athlete().baseWeight}</h4>
                 {/*TODO: Create component for the visualization of data*/}
