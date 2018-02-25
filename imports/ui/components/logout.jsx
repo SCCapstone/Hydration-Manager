ca8367cc8d1b03124b22e9cc9ae218a1cbf2c7a6
afraznein <anthony.frazier1986@gmail.com>
2018-02-25 18:35:22 -0500

    Some work done on logout page. We probably want to port this to a modal window available on each page instead.

--- imports/ui/components/logout.jsx
+++ imports/ui/components/logout.jsx
@@ -6,18 +6,35 @@ export default class Logout extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            showModal: true
+        }
         };
-    };
+
 
     log() {
         Meteor.call('logoutUser', this.props.user._id);
         window.location ='/login';
     }
 
+    open() {
+        this.setState({ showModal: true });
+        this.getCurrentTeam();
+    }
+    close() {
+        this.setState({ showModal: false });
+    }
+
     render() {
         return (
             <div>
-                <Button onClick={this.log}>Not Working Yet</Button>
+                <Modal show={this.state.showModal} onHide={this.close} >
+                    <Modal.Header>
+                        <Modal.Title>Are you sure you want to logout?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Button onClick={this.log}>Not Working Yet</Button>
+                    </Modal.Body>
+                </Modal>
             </div>
         )
     }
