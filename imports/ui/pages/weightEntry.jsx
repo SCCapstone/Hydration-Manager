6c072d95d5ecd5ff711e237babd44ff14ec8bc43
afraznein <anthony.frazier1986@gmail.com>
2018-04-15 16:52:12 -0400

    Initial Pass at Twilio; Adding more 'let' variables to implicitly declared vars; Attempted to fix onClick buttons

--- imports/ui/pages/weightEntry.jsx
+++ imports/ui/pages/weightEntry.jsx
@@ -1,10 +1,10 @@
 // Package Imports
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withTracker } from 'meteor/react-meteor-data';
+import {withTracker} from 'meteor/react-meteor-data';
 import autoBind from 'react-autobind';
-import { Link } from 'react-router-dom'
-import { DropdownButton, MenuItem, Table } from 'react-bootstrap';
+import {Link} from 'react-router-dom'
+import {DropdownButton, MenuItem, Table, ToggleButtonGroup, ToggleButton} from 'react-bootstrap';
 
 // Collection(s) & Custom File(s) Imports
 import WeightDropdownOfTeams from '../components/weightDropdownOfTeams.jsx';
@@ -28,7 +28,7 @@ class WeightEntry extends React.Component {
     };
 
     componentWillUnmount() {
-        this.props.subscriptions.forEach((s) =>{
+        this.props.subscriptions.forEach((s) => {
             s.stop();
         });
     }
@@ -47,15 +47,14 @@ class WeightEntry extends React.Component {
 
     /* handleDebounce function --  prints the state of the selected option within the console log*/
     handleDebounce = () => {
-        console.log('The selected option is:',this.state.selectedOption);
+        console.log('The selected option is:', this.state.selectedOption);
     };
 
     /* handleOptionChange function -- sets selectedOption to e.target.value */
-    handleOptionChange = (e) => {
-        this.setState({selectedOption: e.target.value});
+    handleOptionChange(value){
+        this.setState({selectedOption: value});
         let weightElements = document.getElementsByClassName("weightEnterInput");
-        for(let i=0; i<weightElements.length; i++)
-        {
+        for (let i = 0; i < weightElements.length; i++) {
             weightElements[i].value = "";
             console.log(weightElements[i]);
         }
@@ -65,8 +64,7 @@ class WeightEntry extends React.Component {
     handleSessionChange = (e) => {
         this.setState({selectedSession: e.target.value});
         let weightElements = document.getElementsByClassName("weightEnterInput");
-        for(let i=0; i<weightElements.length; i++)
-        {
+        for (let i = 0; i < weightElements.length; i++) {
             weightElements[i].value = "";
             console.log(weightElements[i]);
         }
@@ -86,20 +84,20 @@ class WeightEntry extends React.Component {
         const curUser = this.props.name;  //CurrentUser.findOne();
         console.log(curUser);
         const id = this.props.userId;  //curUser.userID;
-        return TeamsCollection.find({user:id}).fetch();
+        return TeamsCollection.find({user: id}).fetch();
     };
 
     /* Athletes component */
     athletes() {
         let currentTeam = "";
-        const curUser =  this.props.name;//CurrentUser.findOne(); curUser never used -anthony
+        const curUser = this.props.name;//CurrentUser.findOne(); curUser never used -anthony
         const id = this.props.userId;  //curUser.userID;
-        if(this.props.match.params.teamId) {
+        if (this.props.match.params.teamId) {
             let teamId = this.props.match.params.teamId;
-            currentTeam = TeamsCollection.findOne({"_id": teamId, user:id});
+            currentTeam = TeamsCollection.findOne({"_id": teamId, user: id});
             return AthletesCollection.find({teamId: currentTeam._id}).fetch();
         }
-        else{
+        else {
             return AthletesCollection.find().fetch();
         }
     };
@@ -107,14 +105,14 @@ class WeightEntry extends React.Component {
     /* displayAthletes component */
     displayAthletes() {
         /* If the athletes result is NOT null the athlete single is returned. */
-        if(this.athletes() != null) {
+        if (this.athletes() != null) {
             return (this.athletes().map((athlete) => {
                 return <AthleteEntryList key={athlete._id} athlete={athlete} selOp={this.state.selectedOption}
-                                         session = {this.state.selectedSession} dat={this.state.selectedDate}/>
+                                         session={this.state.selectedSession} dat={this.state.selectedDate}/>
             }))
         }
         /* If nothing else, a tuple stating 'select a team' is returned. */
-        else{
+        else {
             return <li>Select a Team</li>
         }
     }
@@ -124,13 +122,13 @@ class WeightEntry extends React.Component {
         /* If this.props.match.params.teamId, this is set as the teamId. The currentTeam is
          * set to the team of one of the team id. It is finally returns the currentTeam name
          * and currentTeam season. */
-        if(this.props.match.params.teamId) {
+        if (this.props.match.params.teamId) {
             let teamId = this.props.match.params.teamId;
             let currentTeam = TeamsCollection.findOne({"_id": teamId});
             return ": " + currentTeam.name + " " + currentTeam.season;
         }
         /* In other case, an empty string is returned. */
-        else{
+        else {
             return "";
         }
     }
@@ -143,54 +141,50 @@ class WeightEntry extends React.Component {
             <div>
                 <div className="WeightHeader">
                     <h3>Weight Entry {this.displayCurrentTeam()}</h3>
-                    <DropdownButton id={'Team Select'} title={'Team Select'} key={null} bsStyle={'default'}>
-                        {this.teams().map((team) => {
-                            return <WeightDropdownOfTeams key={team._id} team={team}/>
-                        })}
-                        <MenuItem>
-                            <Link to ={ {pathname: "/app/weightEntry/"} }> All Athletes </Link>
-                        </MenuItem>
-                    </DropdownButton>
+                    <div className="WeightButtons">
+                        <input type="date" value={this.state.selectedDate} onChange={this.handleDateChange}
+                               id="DatePicker"/>
+                        <ToggleButtonGroup type="radio" name="options" id="RadioButtons"
+                                           value={this.state.selectedOption}>
+                            <ToggleButton value={"PreWeight"}
+                                          onClick={() => this.handleOptionChange("PreWeight")}>PreWeight</ToggleButton>
+                            <ToggleButton value={"PostWeight"}
+                                          onClick={() => this.handleOptionChange("PostWeight")}>PostWeight</ToggleButton>
+                        </ToggleButtonGroup>
+                        <DropdownButton id={'Team Select'} title={'Team Select'} key={null} bsStyle={'default'}>
+                            {this.teams().map((team) => {
+                                return <WeightDropdownOfTeams key={team._id} team={team}/>
+                            })}
+                            <MenuItem>
+                                <Link to={{pathname: "/app/weightEntry/"}}> All Athletes </Link>
+                            </MenuItem>
+                        </DropdownButton>
+                    </div>
+                    <hr/>
+                    <form>
+                        <br/>
+                        <div>
+                            <select onChange={this.handleSessionChange.bind(this)}>
+                                <option value="1">Session 1</option>
+                                <option value="2">Session 2</option>
+                                <option value="3">Session 3</option>
+                            </select>
+                        </div>
+                        <br/><br/>
+                        <Table striped bordered condensed hover className="teams">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Weight Entry</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.displayAthletes()}
+                            {/*{this.athletes().map((athlete)=>{return <AthleteEntryList key={athlete._id} athlete={athlete} selOp={this.state.selectedOption} dat={this.state.selectedDate}/>})}*/}
+                            </tbody>
+                        </Table>
+                    </form>
                 </div>
-                <hr/>
-                <form>
-                    <br/>
-                    <div className="WeightRadioButtons">
-                        <input type="date" value={this.state.selectedDate} onChange={this.handleDateChange}/>
-                        <label>
-                            <input type="radio" value="PreWeight"
-                                   checked={this.state.selectedOption === 'PreWeight'}
-                                   onChange={this.handleOptionChange}/>
-                            PreWeight
-                        </label>
-                        <label>
-                            <input type="radio" value="PostWeight"
-                                   checked={this.state.selectedOption === 'PostWeight'}
-                                   onChange={this.handleOptionChange.bind(this)}/>
-                            PostWeight
-                        </label>
-                    </div>
-                    <div>
-                        <select onChange={this.handleSessionChange.bind(this)}>
-                            <option value = "1">Session 1</option>
-                            <option value = "2">Session 2</option>
-                            <option value = "3">Session 3</option>
-                        </select>
-                    </div>
-                    <br/><br/>
-                    <Table striped bordered condensed hover className="teams">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Weight Entry</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.displayAthletes()}
-                        {/*{this.athletes().map((athlete)=>{return <AthleteEntryList key={athlete._id} athlete={athlete} selOp={this.state.selectedOption} dat={this.state.selectedDate}/>})}*/}
-                        </tbody>
-                    </Table>
-                </form>
             </div>
         )
     }
