5cc37bd1112188444ff2ce4a4e4212c19dc84bf8
afraznein <anthony.frazier1986@gmail.com>
2018-04-16 15:34:47 -0400

    Create new TODO associated with

--- imports/ui/pages/yourTeams.jsx
+++ imports/ui/pages/yourTeams.jsx
@@ -9,7 +9,7 @@ import {Button, FormControl, FormGroup, Modal} from 'react-bootstrap';
 import TeamsCollection from '../../api/Teams/Teams.js';
 import ListOfTeams from '../components/listOfTeams.jsx';
 
-
+{/*TODO Conditional Rendering of Create a Team button iff user is an admin: See line #82 in this file*/}
 class YourTeams extends React.Component {
     constructor(props) {
         super(props);
@@ -72,13 +72,15 @@ class YourTeams extends React.Component {
         e.persist();
         this.setState({teamSeason: e.target.value});
     };
-
     render() {
         return (
             <div>
                 <div className="YourTeamHeader">
                     <h3>Your Teams</h3>
+
+                    {props.userRoles[0] === "ADMIN" ?
                     <Button onClick={this.open} bsStyle="primary">&#43; Create a Team</Button>
+                    : ''}
                 </div>
                 <hr/>
                 <div>
@@ -114,6 +116,7 @@ class YourTeams extends React.Component {
 }
 
 YourTeams.propTypes = {
+    userRoles: PropTypes.array.isRequired,
     subscriptions: PropTypes.array,
     loading: PropTypes.bool,
     teamsList: PropTypes.array
