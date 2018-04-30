53ff3f7c6d79ce407989346e1f55c15d2ca445ec
meldash <jwdash@email.sc.edu>
2018-04-29 22:56:09 -0400

    Made a couple of adjustments to my last push, so test will run in terminal instead of solely on the browser.

--- imports/api/Athletes/Athletes.test.js
+++ imports/api/Athletes/Athletes.test.js
@@ -4,18 +4,29 @@ import { Random } from 'meteor/random';
 import { AthletesCollection } from './Athletes';
 import { methods } from '../Athletes/methods.js';
 
+/* By default the test will run as both a server and client,
+    I set the test report as a server */
 if(Meteor.isClient) return false;
+
 const userId = Random.id();
 const randTeamId = Random.id();
 let athleteId;
 
+/*Athlete Collection Testing*/
 describe('Athlete Server Test', function(){
+    //Initial Checks
     describe('Running the basics', function(){
+        //Check to see if Server is running
         it('Server Running', function(){});
+        //Check to see if Roles collection exists
         it('Can see collection', function(){});
     });
+
+    //For Testing Database Methods
     describe('Testing Methods...', function(){
+        //Testing the addition of an athlete
         it('Athlete Insert function', function(){});
+        //Testing the edit of an athlete
         it('Athlete Edit function', function(){});
 
         // beforeEach(() => {
@@ -29,6 +40,7 @@ describe('Athlete Server Test', function(){
         //     });
         // });
 
+        //Testing the deletion of an athlete
         it('Athlete Delete function', function(){
             // const deleteAthlete = Meteor.server.method_handlers['athletes.remove'];
             // const invocation = { userId };
--- imports/api/Teams/Teams.test.js
+++ imports/api/Teams/Teams.test.js
@@ -4,19 +4,27 @@ import { Teams } from './Teams';
 /*import { TeamsMethods } from '../Teams/methods.js';*/
 import { Random } from 'meteor/random';
 
-let aName, aWeight, aTeamId;
+/* By default the test will run as both a server and client,
+    I set the test report as a server */
+if(Meteor.isClient) return false;
 
-
-if(Meteor.isServer) return true;
-
+/*Teams Collection Testing*/
 describe('Team Server Test', function(){
+    //Initial Checks
     describe('Running the basics', function(){
+        //Check to see if Server is running
         it('Server Running', function(){});
+        //Check to see if Teams collection exists
         it('Can See Teams Collection', function(){});
     });
+
+    //For Testing Database Methods
     describe('Testing Methods...', function(){
+        //Testing the addition of a new team
         it(' Team Insert function', function(){});
+        //Testing the edit of a team
         it(' Team Edit function', function(){});
+        //Testing the removal of a team
         it(' Team Remove function', function(){});
     });
 });
