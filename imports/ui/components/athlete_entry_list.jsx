db3821de93404f72601d6028c9465cb90fc6fc8e
George Clayton <georgejc@email.sc.edu>
2018-04-30 20:03:35 -0400

    Athletes now only show up in master report and weight entry if you are an admin or if you have permission to view that player's team

--- imports/ui/components/athlete_entry_list.jsx
+++ imports/ui/components/athlete_entry_list.jsx
@@ -257,11 +257,31 @@ export default class AthleteEntryList extends Component {
         if (event.which === 13 /* Enter */) {
             event.preventDefault();
         }
+    };
+
+    handleView() {
+        let currentUser = Meteor.user();
+        if (currentUser !== null) {
+            let check = false;
+            for (let i=0; i < currentUser.profile.teamAccess.length; i++) {
+                if (this.props.athlete.teamId === currentUser.profile.teamAccess[i])
+                {
+                    check = true;
+                }
+            }
+            return check;
+        }
+        else return false;
     }
-    ;
 
     /*Render weights and allows changes*/
     render() {
+
+        if(this.handleView() == false && Meteor.user().roles[0] !== "ADMIN")
+        {
+            return null;
+        }
+
         return (
             <tr>
                 <td>{this.props.athlete.name}</td>
